dee1f113df385e6220b2921d96fc532fbb5bd484
dblinkhorn <dblinkhorn@gmail.com>
2020-08-30 21:13:32 -0700

    added new post functionality within app

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
+# function view
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+# class-based view, posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+# class-based view, details (single-post)
+class PostDetailView(DetailView):
+    model = Post
+
+
+# class-based view, create new post)
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
